5a024bce923e2be453753b2923b07ecc6f12199b
Owen Moss <33687855+OKMantis@users.noreply.github.com>
2025-11-18 16:34:09 +0100

    finished crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  resources :tasks
+  # get "tasks", to: "tasks#index"
+  # get "tasks/new", to: "tasks#new", as: :new_task
+  # post "tasks", to: "tasks#create"
+  # get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  # get "tasks/:id", to: "tasks#show", as: :task
+  # patch "tasks/:id", to: "tasks#update"
+  # delete "tasks/:id", to: "tasks#destroy"
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
